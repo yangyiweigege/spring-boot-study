e2f75b573e8c878f4cb1e0c762db0f7328cd2a18
yangyiwei <yangyiwei@LAPTOP-V8JGD5NF>
2018-06-01 09:50:28 +0800

    ==========

---
+++ spring-boot-study/src/main/java/com/weige/ssm/dao/StudentMapper.java
@@ -0,0 +1,7 @@
+package com.weige.ssm.dao;
+
+import com.weige.ssm.domain.Student;
+import com.weige.ssm.plungin.MyMapper;
+
+public interface StudentMapper extends MyMapper<Student> {
+}
---
+++ spring-boot-study/src/main/java/com/weige/ssm/domain/Student.java
@@ -0,0 +1,54 @@
+package com.weige.ssm.domain;
+
+import javax.persistence.*;
+
+public class Student {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String desc;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return desc
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * @param desc
+     */
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
